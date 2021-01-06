6d40a825618e5c21dae9b65ee8a45760a7c7373e
crystal-csete <crystal-csete@lambdastudents.com>
2021-01-06 15:46:18 -0700

    added

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -2,9 +2,10 @@ import axios from 'axios'
 
 export const axiosWithAuth = () => {
     const token = localStorage.getItem("token");
+    console.log(token)
 
     return axios.create({
-        baseURL: "http://dtebo-expatbackend.herokuapp.com",
+        baseURL: "https://dtebo-expatbackend.herokuapp.com",
         headers: {
             Authorization:  `Basic ${btoa("lambda-client:lambda-secret")}`,
             "Content-Type": "application/x-www-form-urlencoded",
